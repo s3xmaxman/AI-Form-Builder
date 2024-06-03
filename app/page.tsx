0817ec5e9079a187bfbb1ed92aab47ffb60f73e4
s3xmaxman <badside3919@gmail.com>
2024-06-04 03:35:12 +0900

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,18 @@ import Image from "next/image";
 import FormGenerator from "./form-generator";
 import Header from "@/components/ui/header";
 import { SessionProvider } from "next-auth/react";
+import { db } from "@/db";
+import FormsList from "./forms/FormsList";
 
-export default function Home() {
+export default async function Home() {
+  const forms = await db.query.forms.findMany();
   return (
     <>
       <SessionProvider>
         <Header />
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center p-24">
           <FormGenerator />
+          <FormsList forms={forms} />
         </main>
       </SessionProvider>
     </>
