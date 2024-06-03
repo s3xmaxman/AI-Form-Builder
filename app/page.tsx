2db9a19b87c1da940e6481fabd7065dbbb7a93a7
s3xmaxman <badside3919@gmail.com>
2024-06-03 20:00:11 +0900

    update page.tsx and next.config.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import FormGenerator from "./form-generator";
+import Header from "@/components/ui/header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <FormGenerator />
-    </main>
+    <>
+      <Header />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <FormGenerator />
+      </main>
+    </>
   );
 }
