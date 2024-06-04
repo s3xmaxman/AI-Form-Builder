3a0c9103f1418c1abaa85fb57ef3fcd64b156b7b
s3xmaxman <badside3919@gmail.com>
2024-06-05 07:51:09 +0900

    update Form.tsx

--- app/forms/Form.tsx
+++ app/forms/Form.tsx
@@ -12,12 +12,12 @@ import {
   FormLabel,
   FormControl,
 } from "@/components/ui/form";
-import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
-import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 import FormField from "./FormField";
 import { publishForm } from "../actions/mutationForm";
 import FormPublishSuccess from "./FormPublishSuccess";
+import { useRouter } from "next/navigation";
 
 type Props = {
   form: Form;
@@ -47,8 +47,45 @@ const Form = (props: Props) => {
     if (editMode) {
       await publishForm(props.form.id);
       setSuccessDialogOpen(true);
+    } else {
+      let answers = [];
+      for (const [questionId, value] of Object.entries(data)) {
+        const id = parseInt(questionId.replace("question_", ""));
+        let fieldOptionsId = null;
+        let textValue = null;
+
+        if (typeof value == "string" && value.includes("answerId_")) {
+          fieldOptionsId = parseInt(value.replace("answerId_", ""));
+        } else {
+          textValue = value as string;
+        }
+
+        answers.push({
+          questionId: id,
+          fieldOptionsId,
+          value: textValue,
+        });
+      }
+
+      const baseUrl =
+        process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+
+      const response = await fetch(`${baseUrl}/api/form/new`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ formId: props.form.id, answers }),
+      });
+      if (response.status === 200) {
+        router.push(`/forms/${props.form.id}/success`);
+      } else {
+        console.error("Error submitting form");
+        alert("Error submitting form. Please try again later");
+      }
     }
   };
+
   return (
     <div className="text-center">
       <h1 className="text-lg font-bold py-3">{props.form.name}</h1>
